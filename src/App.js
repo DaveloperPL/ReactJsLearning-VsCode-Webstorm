a3f3a38b19cae0459f7b2352110628b9a340a680
David <davidmroz101@gmail.com>
2024-09-30 08:04:41 -0500

    Bootstrap setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+// src/index.js or src/App.js
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className='my-4'>
+          Edit <code className='my'>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -21,5 +25,7 @@ function App() {
     </div>
   );
 }
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 export default App;
